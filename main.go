11c3e7d34e48aba13cc125d0a7ce5a9dcee9ee75
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2025-05-27 20:10:27 +0300

    social handlermux

--- main.go
+++ main.go
@@ -1,40 +1,89 @@
-// package main tells go that this is an application and not a library hence look for the main function to start
+// // package main tells go that this is an application and not a library hence look for the main function to start
+// package main
+
+// // fmt is Go's standard formatting package, that help you print to the console
+// // net/http is the http server and client library, defines request handlers and start an http server
+// import (
+// 	"fmt"
+// 	"net/http"
+// 	"log"
+// )
+
+// // the line below declares a new type called server, and in it addr is a string
+// // by storing the address in a struct, it not only stores the address but also anything else the handlers need like DB connection, logger instance, etc.
+// type server struct {
+// 	addr string
+// }
+
+// // in the function below the server type is attached to the ServeHTTP function, hence *server is a valid HTTP handler
+// // the function below is the main entry point for every incoming request, whenever someone does sth like GET http://localhost:8081/foo, GO's HTTP server will call the serverHttp method, and pass in the response writer w to write back headers and body, also the r Request object describing the incoming request
+// // the method below is similar to app.use((req, res))
+// // any function that looks like the two below, those are handlers, and they are callbacks that HTTP server invokes for every incoming request
+// // func(w http.ResponseWriter, r *http.Request) OR
+// // ServeHTTP(w http.ResponseWriter, r *http.Request)
+
+// func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	// HTTP sends raw bytes over the wire hence convert GO string to []byte a format that is required
+// 	w.Write([]byte("Hello Server running on port " + s.addr))
+// 	fmt.Println("Hello World")
+// }
+
+// func main() {
+
+// 	// instance of the server, with value ":8081" from addr variable
+// 	s := &server{addr: ":8081"}
+// 	// pass the s in place of the nil handler. Go sees the s has a serveHttp method, so it'll call the s.ServeHTTP for every request
+// 	err :=http.ListenAndServe(s.addr, s)
+// 	if err !=nil{
+// 		log.Fatal(err)
+// 	}
+// }
+
+
+// REFACTOR
+
 package main
 
-// fmt is Go's standard formatting package, that help you print to the console
-// net/http is the http server and client library, defines request handlers and start an http server
-import (
-	"fmt"
-	"net/http"
-	"log"
-)
+import "net/http"
 
-// the line below declares a new type called server, and in it addr is a string
-// by storing the address in a struct, it not only stores the address but also anything else the handlers need like DB connection, logger instance, etc.
-type server struct {
+type api struct{
 	addr string
 }
 
-// in the function below the server type is attached to the ServeHTTP function, hence *server is a valid HTTP handler
-// the function below is the main entry point for every incoming request, whenever someone does sth like GET http://localhost:8081/foo, GO's HTTP server will call the serverHttp method, and pass in the response writer w to write back headers and body, also the r Request object describing the incoming request
-// the method below is similar to app.use((req, res))
-// any function that looks like the two below, those are handlers, and they are callbacks that HTTP server invokes for every incoming request
-// func(w http.ResponseWriter, r *http.Request) OR
-// ServeHTTP(w http.ResponseWriter, r *http.Request)
-
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// HTTP sends raw bytes over the wire hence convert GO string to []byte a format that is required
-	w.Write([]byte("Hello Server running on port " + s.addr))
-	fmt.Println("Hello World")
+func (s *api) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	switch r.Method{
+	case http.MethodGet:
+		switch r.URL.Path{
+		case "/":
+			w.Write([]byte("Get Method"))
+		}
+	case "/index":
+		w.Write([]byte("Index"))
+	case http.MethodPost:
+		w.Write([]byte("Post Method"))
+	}
 }
 
-func main() {
+func (a *api) createUsersHandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Create User"))
+}
 
-	// instance of the server, with value ":8081" from addr variable
-	s := &server{addr: ":8081"}
-	// pass the s in place of the nil handler. Go sees the s has a serveHttp method, so it'll call the s.ServeHTTP for every request
-	err :=http.ListenAndServe(s.addr, s)
-	if err !=nil{
-		log.Fatal(err)
+func (a *api) getUsershandler(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Get User..."))
+}
+
+func main(){
+	api := &api{addr:"8081"}
+	// a mux is a router
+	mux :=http.NewServeMux()
+	srv := &http.Server{
+		Addr: api.addr,
+		Handler: mux,
 	}
+
+	mux.HandleFunc("GET /users", api.getUsershandler)
+	//mux needs a string and a handlerFunc as its arguments
+	mux.HandleFunc("POST /users", api.createUsersHandler)
+
+	srv.ListenAndServe()
 }
--- others/interface.go
+++ others/interface.go
@@ -8,13 +8,16 @@ type Speaker interface {
 	Speak() string
 }
 
-// Define a struct
+// Define a struct, object called Dog, with a field called name of type string
+//structs in Go, are used to group data together
 type Dog struct {
 	Name string
 }
 
 // Dog implements Speak()
-// the (d Dog) is called the receiver, its how Go associates this function qith the Dog type.
+// the (d Dog) is called the receiver, its how Go associates this function with the Dog type.
+// the d Dog means that the Speak method belongs to the type Dog, and d is the current instance inside the method.
+//(d Dog) is the receiver type, Speak is the method name, that can accept parameters String is the return type.
 func (d Dog) Speak() string {
 	return d.Name + " says Woof!"
 }
@@ -29,7 +32,7 @@ func (c Cat) Speak() string {
 	return c.Name + " says Meow!"
 }
 
-// A function that accepts anything that implements Speaker
+// A function that accepts anything that implements Speaker interface (ie. Anything with a Speak string method)
 func makeSpeak(s Speaker) {
 	fmt.Println(s.Speak())
 }
